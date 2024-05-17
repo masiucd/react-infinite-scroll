f59393724c3fe21ec341ae1adc3e618a9b4baf7e
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2024-05-17 15:38:27 +0200

    chore: Update npm dependencies and add infinite scroll functionality using React Query and Zod

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {
   QueryClientProvider,
   useQuery,
 } from "@tanstack/react-query";
+import {z} from "zod";
 
 let queryClient = new QueryClient();
 function App() {
@@ -13,27 +14,46 @@ function App() {
   );
 }
 
-export default App;
+let postSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+  body: z.string(),
+});
 
 function Main() {
   let {data, isError, isPending} = useQuery({
     queryKey: ["posts"],
     queryFn: fetchPosts,
   });
+  if (isError) {
+    return <p>Error fetching posts</p>;
+  }
   return (
     <div>
       <h1 className="mt-10 text-center text-4xl font-bold">
         React infinite scroll
       </h1>
-      <ul>{isPending && <p>Loading...</p>}</ul>
+      <ul>
+        {isPending ? (
+          <p>Loading...</p>
+        ) : (
+          data?.map((post) => (
+            <li key={post.id} className="border-b-2 border-gray-200 p-4">
+              <h2 className="text-xl font-bold">{post.title}</h2>
+              <p>{post.body}</p>
+            </li>
+          ))
+        )}
+      </ul>
     </div>
   );
 }
 
 const BASE_URL = "https://jsonplaceholder.typicode.com/posts";
 async function fetchPosts() {
-  let response = await fetch(`${BASE_URL}`);
+  let response = await fetch(`${BASE_URL}?_start=0&_end=10`);
   let posts = await response.json();
-
-  return posts;
+  return postSchema.array().parse(posts);
 }
+
+export default App;
