937a09c802fdc948b216335746ac5f10f94e45b1
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2024-05-17 15:34:08 +0200

    chore: Add infinite scroll functionality using React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+
+let queryClient = new QueryClient();
 function App() {
   return (
-    <>
-      <h1 className="text-4xl text-center font-bold mt-10">
-        React infinite scroll
-      </h1>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Main />
+    </QueryClientProvider>
   );
 }
 
 export default App;
+
+function Main() {
+  let {data, isError, isPending} = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+  return (
+    <div>
+      <h1 className="mt-10 text-center text-4xl font-bold">
+        React infinite scroll
+      </h1>
+      <ul>{isPending && <p>Loading...</p>}</ul>
+    </div>
+  );
+}
+
+const BASE_URL = "https://jsonplaceholder.typicode.com/posts";
+async function fetchPosts() {
+  let response = await fetch(`${BASE_URL}`);
+  let posts = await response.json();
+
+  return posts;
+}
