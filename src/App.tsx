eb0a0378853e6b6a76960cb8649669694629957b
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2024-05-17 10:06:09 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-center font-bold mt-10">
+        React infinite scroll
+      </h1>
+    </>
+  );
+}
+
+export default App;
