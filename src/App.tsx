a875e6ffff806f4435694d70e6898140b9566a40
masiucd <ciszekmarcell@gmail.com>
2024-05-18 18:03:48 +0200

    chore: Implement lazy loading for images and infinite scroll functionality using React Query and Intersection Observer

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,22 @@ import {
   useQuery,
 } from "@tanstack/react-query";
 import {useEffect, useState} from "react";
+import {useInView} from "react-intersection-observer";
 import {z} from "zod";
 
 let queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <header className="h-20">
+        <strong>
+          Infinite Scroll with Tanstack Query and Intersection Observer
+        </strong>
+      </header>
       <Main />
+      <footer className="h-20 ">
+        <small></small>
+      </footer>
     </QueryClientProvider>
   );
 }
@@ -21,14 +30,15 @@ let postSchema = z.object({
   body: z.string(),
 });
 
-const OFFSET = 4;
+const OFFSET = 10;
 
 function Main() {
   let [end, setEnd] = useState(OFFSET);
-  // let [posts, setPosts] = useState<z.infer<typeof postSchema>[] | never[]>([]);
-  let {data, isError, isPending, refetch} = useQuery({
+  let {ref, inView} = useInView({
+    threshold: 1,
+  });
+  let {data, isError, isPending, refetch, isLoading, isFetching} = useQuery({
     queryKey: ["posts"],
-
     queryFn: async () => {
       let {posts, totalPosts} = await fetchPosts(0, end);
       return {posts, totalPosts};
@@ -36,37 +46,36 @@ function Main() {
   });
 
   useEffect(() => {
+    if (inView) {
+      setEnd((prev) => prev + OFFSET);
+    }
+  }, [inView]);
+
+  useEffect(() => {
     refetch();
   }, [refetch, end]);
 
-  console.log({end, data: data?.posts});
   if (isError) {
     return <p>Error fetching posts</p>;
   }
 
   return (
-    <div>
+    <main className="flex min-h-[calc(100dvh-10rem)] flex-col border bg-blue-400">
       <h1 className="mt-10 text-center text-4xl font-bold">
         React infinite scroll
       </h1>
-      {isPending ? (
-        <p>Loading...</p>
-      ) : (
-        <Posts posts={data?.posts.success ? data?.posts.data : []} />
-      )}
-      <div>
-        {data?.totalPosts && (
-          <button
-            onClick={() => {
-              setEnd((prev) => prev + OFFSET);
-            }}
-            className="rounded-md bg-blue-500 p-2 text-white"
-          >
-            Load More
-          </button>
+      <div className="mb-4 flex-1 border border-red-500 bg-green-400 p-4">
+        {isPending ? (
+          <p>Loading...</p>
+        ) : (
+          <Posts posts={data?.posts.success ? data?.posts.data : []} />
         )}
       </div>
-    </div>
+      {(isLoading || isPending || isFetching) && <p>Loading...</p>}
+      <div ref={ref} className="h-20 p-10">
+        <p>In view {inView.toString()}</p>
+      </div>
+    </main>
   );
 }
 
@@ -74,7 +83,7 @@ function Posts({posts}: {posts: z.infer<typeof postSchema>[] | never[]}) {
   return (
     <ul>
       {posts.map((post) => (
-        <li key={post.id} className="border-b-2 border-gray-200 p-4">
+        <li key={post.id} className="border-b-2 border-gray-200 px-4 py-8">
           <h2 className="text-xl font-bold">{post.title}</h2>
           <p>{post.body}</p>
         </li>
@@ -83,12 +92,16 @@ function Posts({posts}: {posts: z.infer<typeof postSchema>[] | never[]}) {
   );
 }
 
+async function sleep(time: number) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
 const BASE_URL = "https://jsonplaceholder.typicode.com/posts";
 async function fetchPosts(start = 0, end = 10) {
-  console.log({start, end});
   let response = await fetch(`${BASE_URL}?_start=${start}&_end=${end}`);
   let posts = await response.json();
   let totalPosts = response.headers.get("X-Total-Count");
+  await sleep(4000);
   return {
     posts: postSchema.array().safeParse(posts),
     totalPosts,
