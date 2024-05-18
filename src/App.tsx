b21f6af8ec6228c5dff4e8792290b05728bafa82
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2024-05-18 09:23:53 +0200

    chore: Add lazy loading for images and implement infinite scroll functionality using React Query and Zod

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {
   QueryClientProvider,
   useQuery,
 } from "@tanstack/react-query";
+import {useState} from "react";
 import {z} from "zod";
 
 let queryClient = new QueryClient();
@@ -21,39 +22,71 @@ let postSchema = z.object({
 });
 
 function Main() {
+  let [start, setStart] = useState(0);
+  let [end, setEnd] = useState(10);
+  let [posts, setPosts] = useState<z.infer<typeof postSchema>[] | never[]>([]);
+  console.log({start, end});
   let {data, isError, isPending} = useQuery({
     queryKey: ["posts"],
-    queryFn: fetchPosts,
+    queryFn: async () => {
+      let {posts, totalPosts} = await fetchPosts(start, end);
+      return {posts, totalPosts};
+    },
   });
   if (isError) {
     return <p>Error fetching posts</p>;
   }
+
   return (
     <div>
       <h1 className="mt-10 text-center text-4xl font-bold">
         React infinite scroll
       </h1>
-      <ul>
-        {isPending ? (
-          <p>Loading...</p>
-        ) : (
-          data?.map((post) => (
-            <li key={post.id} className="border-b-2 border-gray-200 p-4">
-              <h2 className="text-xl font-bold">{post.title}</h2>
-              <p>{post.body}</p>
-            </li>
-          ))
+      {isPending ? (
+        <p>Loading...</p>
+      ) : (
+        <Posts posts={data?.posts.success ? data?.posts.data : []} />
+      )}
+      <div>
+        {data?.totalPosts && (
+          <button
+            onClick={() => {
+              setStart(start + 10);
+              setEnd(end + 10);
+            }}
+            className="rounded-md bg-blue-500 p-2 text-white"
+          >
+            Load More
+          </button>
         )}
-      </ul>
+      </div>
     </div>
   );
 }
 
+function Posts({posts}: {posts: z.infer<typeof postSchema>[] | never[]}) {
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id} className="border-b-2 border-gray-200 p-4">
+          <h2 className="text-xl font-bold">{post.title}</h2>
+          <p>{post.body}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
 const BASE_URL = "https://jsonplaceholder.typicode.com/posts";
-async function fetchPosts() {
-  let response = await fetch(`${BASE_URL}?_start=0&_end=10`);
+async function fetchPosts(start = 0, end = 10) {
+  console.log({start, end});
+  let response = await fetch(`${BASE_URL}?_start=${start}&_end=${end}`);
   let posts = await response.json();
-  return postSchema.array().parse(posts);
+  let totalPosts = response.headers.get("X-Total-Count");
+  return {
+    posts: postSchema.array().safeParse(posts),
+    totalPosts,
+  };
 }
 
 export default App;
